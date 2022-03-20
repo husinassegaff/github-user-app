29bbaf47e4bc57e2e220402524c039ba8cf16f44
husinassegaff <husinassegaff15@gmail.com>
2022-03-20 22:02:17 +0700

    feat: change findviewbyId to binding for adapter

--- app/src/main/java/com/example/githubuserapp/ListUserAdapter.kt
+++ app/src/main/java/com/example/githubuserapp/ListUserAdapter.kt
@@ -1,11 +1,9 @@
 package com.example.githubuserapp
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.githubuserapp.databinding.ItemRowUserBinding
 
 class ListUserAdapter(private val listUser: ArrayList<User>) : RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
 
@@ -16,30 +14,32 @@ class ListUserAdapter(private val listUser: ArrayList<User>) : RecyclerView.Adap
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_user, parent, false)
-        return ListViewHolder(view)
+        val binding = ItemRowUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name, username, description, location, avatar) = listUser[position]
-        holder.tvName.text = name
-        holder.tvUsername.text = username
-        holder.tvDescription.text = description
-        holder.tvLocation.text = location
-        holder.imgPhoto.setImageResource(avatar)
+        holder.binding.tvItemName.text = name
+        holder.binding.tvItemUsername.text = username
+        holder.binding.tvItemDescription.text = description
+        holder.binding.tvItemLocation.text = location
+        holder.binding.imgItemPhoto.setImageResource(avatar)
 
-        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listUser[holder.adapterPosition]) }
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(listUser[holder.adapterPosition])
+        }
     }
 
     override fun getItemCount(): Int = listUser.size
 
 
-    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var tvName : TextView = itemView.findViewById(R.id.tv_item_name)
-        var tvUsername: TextView = itemView.findViewById(R.id.tv_item_username)
-        var tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
-        var tvLocation: TextView = itemView.findViewById(R.id.tv_item_location)
-        var imgPhoto: ImageView =  itemView.findViewById(R.id.img_item_photo)
+    class ListViewHolder(var binding: ItemRowUserBinding) : RecyclerView.ViewHolder(binding.root) {
+//        var tvName : TextView = itemView.findViewById(R.id.tv_item_name)
+//        var tvUsername: TextView = itemView.findViewById(R.id.tv_item_username)
+//        var tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
+//        var tvLocation: TextView = itemView.findViewById(R.id.tv_item_location)
+//        var imgPhoto: ImageView =  itemView.findViewById(R.id.img_item_photo)
     }
 
     interface OnItemClickCallback {
